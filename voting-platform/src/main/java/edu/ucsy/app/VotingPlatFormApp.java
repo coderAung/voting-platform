16028757c097af3c9fd667fb91bc64f0ba89f625
coderAung <yewontaung798@gmail.com>
2026-02-01 22:48:45 +0630

    add dependencies

--- voting-platform/src/main/java/edu/ucsy/app/VotingPlatFormApp.java
+++ voting-platform/src/main/java/edu/ucsy/app/VotingPlatFormApp.java
@@ -1,4 +1,24 @@
 package edu.ucsy.app;
 
-public class VotingPlatFormApp {
+import edu.ucsy.app.ui.controller.Home;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class VotingPlatFormApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        var view = (Parent) FXMLLoader.load(Objects.requireNonNull(Home.class.getResource("Home.fxml")));
+        stage.setScene(new Scene(view));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- voting-platform/src/main/java/module-info.java
+++ voting-platform/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module edu.ucsy.app.votingplatform {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens edu.ucsy.app to javafx.fxml;
+    opens edu.ucsy.app.ui.controller to javafx.fxml;
+
     exports edu.ucsy.app;
 }
